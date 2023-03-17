62e700fc118972e0bbd252da58cfbb159d69e095
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-03-16 19:31:41 -0700

    C unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main A - printing without using printf and puts
+ * main - A program that prints a line to the standard error
  *
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(STDOUT_FILENO, art, 59);
 	return (1);
 }
-
