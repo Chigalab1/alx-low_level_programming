814c83c17f465607d2a435ff12778f90edac0852
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-03-16 17:09:40 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - A script that prints a line using the function puts
- * Return : 0 (Success)
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
