47394f665c365dde695369a3a6d5f47265e56771
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-03-21 17:44:31 -0700

    jolly

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints last digit of a number
+ *
+ * @n: function parameter
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int k;
+
+	k = n % 10;
+	if (n < 0)
+	{
+		k = -k;
+	}
+	_putchar (k + '0');
+	return (k);
+}
