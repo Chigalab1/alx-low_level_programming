efb5e54f98482d4691f0c7b57667da2e14b6e186
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-04-04 01:34:33 -0700

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int s1 = s2 = 0;
+	int s1 = 0;
+	int s2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		s1 += a[i];
-		s2 += s[size - i - 1];
+		s2 += a[size - i - 1];
 		a += size;
 	}
 	printf("%d, ", s1);
