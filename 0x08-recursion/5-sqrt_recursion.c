59f4270dbf62777e91be02c6a92b98a494b6d960
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-04-05 16:07:34 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,30 @@
 /**
  * _sqrt_recursion - returns the square root if an int
  * @n: number
+ * @i: square root of int
  *
  * Return: square root or -1
  */
+int square(int n, int i);
 int _sqrt_recursion(int n)
 {
+	return (square(n, 1));
+}
 
+/**
+ *square - function that finds square root
+ *@n: number@i: square root of int
+ *@i: square root of int
+ *
+ * Return: integer
+ */
+int square(int n, int i)
+{
+
+	if (i * i == n)
+		return (i);
+	else if (i * i < n)
+		return (square(n, i + 1));
+	else
+		return (-1);
+}
