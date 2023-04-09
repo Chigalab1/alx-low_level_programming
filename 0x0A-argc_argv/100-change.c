dd81f795a7578fc4ede373add7a31d2b7241c83f
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-04-09 14:47:05 -0700

    100-changes

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of arg
+ * @argv: array of arg
+ *
+ * Return: 0 on succes or 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int n, i, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+	result = 0;
+
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5 && n >= 0; i++)
+	{
+		while (n >= coins[i])
+		{
+			result++;
+			n = n - coins[i];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
