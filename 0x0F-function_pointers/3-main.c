3d1e1fcabc96994268c28b6821c9e016b99b9d51
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-04-20 16:00:42 -0700

    op

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1;
 	int num2;
-	char *op;
+	char *p;
 
 	if (argc != 4)
 	{
@@ -22,30 +22,30 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
+	p = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_0p_func(op) == NULL)
+	if (get_0p_func(p) == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (op[1] != '\0')
+	if (p[1] != '\0')
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (*op == '/' && num2 == 0)
+	if (*p == '/' && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (*op == '%' && num2 == 0)
+	if (*p == '%' && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_0p_func(op)(num1, num2));
+	printf("%d\n", get_0p_func(p)(num1, num2));
 	return (0);
 }
