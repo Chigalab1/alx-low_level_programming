1b836c1a49f57f51db42510c4790dda2badbb8ca
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-05-01 00:01:59 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a linked list
+ * @head: double pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *t;
+
+	if (head == NULL)
+		return;
+	for (; *head != NULL; *head = t)
+	{
+		t = (*head)->next;
+		free(*head);
+	}
+
+	*head == NULL;
+}
