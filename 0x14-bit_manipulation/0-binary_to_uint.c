5e1f0110eb4985da5cbbab70924b970ec96e8c6e
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-05-04 09:19:34 -0700

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - function that converts binary to unsigned int
@@ -18,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] > '1' || b[i] < '0')
 			return (0);
-		decimal = 2 * decima + (b[i] - '0');
+		decimal = 2 * decimal + (b[i] - '0');
 	}
 	return (decimal);
 }
