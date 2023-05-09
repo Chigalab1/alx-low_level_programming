d1ce0c782ebe82303b99f3654aacf7ad059c446f
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-05-08 20:49:43 -0700

    efl update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -236,15 +236,15 @@ void entry_print(unsigned long int e_entry, unsigned char *e_ident)
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
-		e_entry = ((e_entry << 9) & 0xFF00FF00) |
+		e_entry = ((e_entry << 8) & 0xFF00FF00) |
 			((e_entry >> 8) & 0xFF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 
 	if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("%x#\n", (unsigned int)e_entry);
+		printf("%#x\n", (unsigned int)e_entry);
 	else
-		printf("%lx\n", e_entry);
+		printf("%#lx\n", e_entry);
 }
 
 /**
