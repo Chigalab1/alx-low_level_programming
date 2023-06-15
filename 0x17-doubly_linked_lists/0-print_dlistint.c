46721beb751bebb05e11a7a4f0f74819cd32f7ef
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-15 09:31:08 -0700

    print dlist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *temp;
 	size_t count = 0;
 
-	temp = malloc(sizeof(dlistint_t));
 	temp = h;
-
-	if (temp == NULL)
-		return (count);
 
 	while (temp != NULL)
 	{
